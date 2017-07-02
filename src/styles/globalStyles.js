4168c63ba4016a894f629738632c70ab53662944
codenakama <rj.abreu@outlook.com>
2017-07-02 23:25:09 +0100

    list style reset

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -4,4 +4,10 @@ injectGlobal`
   @import 'https://fonts.googleapis.com/css?family=Baloo+Chettan';
 body{
   font-family: 'Baloo Chettan', sans-serif; }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
 `;
